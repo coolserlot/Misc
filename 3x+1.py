bf70009b2fed33f6fa316d660c408e162a45e722
coolserlot <80652991+coolserlot@users.noreply.github.com>
2024-12-05 17:31:13 -0500

    Add files via upload

---
+++ 3x+1.py
@@ -0,0 +1,17 @@
+x=1
+y=x
+z=1
+v=1
+while (z==v):
+    while (y!=1):
+        if (y%2==0):
+            y=y/2
+
+        else:
+            y=(3*y)+1
+     #print(y)
+
+    print (x)
+    print("falls down to one")
+    x=x+1
+
